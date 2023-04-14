aafca0b9c9c0176296fe3ae9387b6b800e0a84c0
Евгений Дитковский <eugene.ditkovsky@gmail.com>
2023-04-14 18:20:10 +0300

    more fixes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-vf(qmeu%%!vhpr*il7x5lz-ee(msaqf5+uk!g1#vdt8og5u3nt'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
