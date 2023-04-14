eb4fd777e5730f71f3256553f751ed7d977daba7
Евгений Дитковский <eugene.ditkovsky@gmail.com>
2023-04-14 20:59:37 +0300

    more fixes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -117,14 +117,14 @@ class RecipeSerializer(serializers.ModelSerializer):
         )
 
     def get_is_favorited(self, obj):
-        user = self.context('request').user
+        user = self.context['request'].user
         if user.is_authenticated:
             return FavoriteRecipe.objects.filter(recipe=obj,
                                                  user=user).exists()
         return False
 
     def get_is_in_shopping_cart(self, obj):
-        user = self.context('request').user
+        user = self.context['request'].user
         if user.is_authenticated:
             return ShoppingCart.objects.filter(recipe=obj,
                                                user=user).exists()
